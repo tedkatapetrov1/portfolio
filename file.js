c28f19aecf5d29ab5c2e093bbcd237445dd00ce6
tedkatapetrov1 <123327479+tedkatapetrov1@users.noreply.github.com>
2023-01-22 21:35:17 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,8 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+function myFunc(){
+              alert("Thanks for visiting my portfolio page! I am so glad that you are here.");
+           }
+
