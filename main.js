030a31892a005cbaf70562b35cb0c4a81e4fd732
Teodor Petrov <123327479+tedkatapetrov1@users.noreply.github.com>
2023-01-25 14:51:10 +0200

    removed spaces

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
+  window.scrollTo(0, 0);
+}
 
-function myFunc(){
-              alert("Thanks for visiting my portfolio page! I am so glad that you are here.");
-           }
-
+function myFunc() {
+  alert("Thanks for visiting my portfolio page! I am so glad that you are here.");
+}
